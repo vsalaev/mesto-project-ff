4c4000a14602f754c9b026d8f7f0a5ea14ab55e8
decider <v@salaev.ru>
2025-01-15 01:16:09 +0300

    fix all first review points

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,42 @@
 const container = document.querySelector('.content');
 const placesList = container.querySelector('.places__list');
 
-function addCard(imageSrc, titleValue) {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+function renderCard(imageSrc, titleValue, method = 'prepend') {
+
+    function deleteButt(deleteButt, cardElement) {
+        deleteButt.addEventListener('click', function (evt) {
+            cardElement.setAttribute('style', 'display:none');
+        });
+    }
+
+    function like(like) {
+        like.addEventListener('click', function (evt) {
+            like.classList.toggle('card__like-button_is-active');
+        });
+    }
     
-    const imageCardEl = cardElement.querySelector('.card__image');
-    const titleCardEl = cardElement.querySelector('.card__title');
-    const likeButtonCardEl = cardElement.querySelector('.card__like-button');
-    const deleteButtonCardEl = cardElement.querySelector('.card__delete-button');
+    function createCard(imageSrc, titleValue, likeButton, deleteButton) {
+        const cardTemplate = document.querySelector('#card-template').content;
+        const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+        
+        const imageCardEl = cardElement.querySelector('.card__image');
+        const titleCardEl = cardElement.querySelector('.card__title');
+            
+        const likeButtonCardEl = cardElement.querySelector('.card__like-button');
+        const deleteButtonCardEl = cardElement.querySelector('.card__delete-button');
+        
+        imageCardEl.setAttribute('src', imageSrc);
+        imageCardEl.setAttribute('alt', titleValue);
+        titleCardEl.textContent = titleValue;
+        
+        likeButton(likeButtonCardEl);
+        deleteButton(deleteButtonCardEl, cardElement);
+        return cardElement;
+    };
 
-    imageCardEl.setAttribute('src', imageSrc);
-    imageCardEl.setAttribute('alt', titleValue);
-    titleCardEl.textContent = titleValue;
+    const cardElement = createCard(imageSrc, titleValue, like, deleteButt);
+    
+    placesList[ method ](cardElement);
+}
 
-    likeButtonCardEl.addEventListener('click', function (evt) { 
-        likeButtonCardEl.classList.toggle('card__like-button_is-active');
-        });
-
-    deleteButtonCardEl.addEventListener('click', function (evt) { 
-        cardElement.setAttribute('style', 'display:none');
-        });
-
-    placesList.append(cardElement);
-};
-
-
-// функция принимает в вызов карточку и метод вставки
-// метод по умолчанию `prepend`, но можно указать `append` 
-
-function renderCard(item, method = "prepend") {
-
-    // создаем карточку, передавая обработчики в виде объекта `callbacks`
-    const cardElement = createCard(item, callbacks);
-  
-    // вставляем карточку, используя метод (вставится `prepend` или `append`)
-    cardList[ method ](cardElement);
-  }
-  
-
-initialCards.forEach(item => addCard(item.link, item.name));
+initialCards.forEach(item => renderCard(item.link, item.name, 'append'));
